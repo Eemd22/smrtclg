d8669980092817c27dac68f07370ffb8745da72b
Eemd22 <emd.sec77@gmail.com>
2026-06-10 22:11:34 +0200

    Update user.controller.js uuuid

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -65,13 +65,14 @@ exports.getAllUserBYId = (req, res) => {
 
 // دالة اضافة مستخدم جديد
 exports.addUser = (req, res, io) => {
-    const { username, email, password } = req.body;
-    const sql = "INSERT INTO users (uuid,username, email, password) VALUES (?,?, ?, ?)";
-    db.query(sql, [uuid,username, email, password], (err, result) => {
-        if (err) return res.status(500).json(err);
-        io.emit("dataChanged");
-        res.json({ message: "added" });
-    });
+	console.log(uuuid);
+    // const { username, email, password } = req.body;
+    // const sql = "INSERT INTO users (uuid,username, email, password) VALUES (?,?, ?, ?)";
+    // db.query(sql, [uuuid,username, email, password], (err, result) => {
+    //     if (err) return res.status(500).json(err);
+    //     io.emit("dataChanged");
+    //     res.json({ message: "added" });
+    // });
 };
 // دالة حذف مستخدم
 exports.deleteUser = (req, res, io) => {
